d61a7afd905bc74c7cbd2e1846d3d90fae8f440e
Eduardo Volken <edupelomundo13@icloud.com>
2026-07-02 08:28:14 +0100

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 // VOLYNX: Cloudflare Pages compatible (no adapter)
 import { defineConfig } from 'astro/config';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
   site: 'https://volynx.world',
   base: '/',
   output: 'static',
-  vite: {},
+  vite: {
+    plugins: [wasm(), topLevelAwait()]
+  },
   markdown: {
     shikiConfig: {
       theme: 'github-dark-dimmed'
     }
   }
 });
-
